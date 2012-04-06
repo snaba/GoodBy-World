f8d856c8f9b95cf2ccababd5c5ee006a286c5353
snaba <satoru_naba@fukuoka-csk.co.jp>
2012-04-06 14:07:35 +0900

    フォルダ構成変更

--- Hello-World/Form1.cpp
+++ Hello-World/Form1.cpp
@@ -1,6 +1,6 @@
 #include "StdAfx.h"
 #include "Form1.h"
-#include "ChildForm1.h"
+#include "Dialogs/ChildForm1.h"
 
 
 System::Void HelloWorld::Form1::button1_Click(System::Object^  sender, System::EventArgs^  e)
