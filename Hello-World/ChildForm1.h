f9d7ac24e693f3e047ca4f2e2c13d1ddb57efdc7
snaba <satoru_naba@fukuoka-csk.co.jp>
2012-04-06 11:34:21 +0900

    子フォーム追加

--- Hello-World/ChildForm1.h
+++ Hello-World/ChildForm1.h
@@ -10,12 +10,12 @@ namespace HelloWorld {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Form1 の概要
+	/// ChildForm1 の概要
 	/// </summary>
-	public ref class Form1 : public System::Windows::Forms::Form
+	public ref class ChildForm1 : public System::Windows::Forms::Form
 	{
 	public:
-		Form1(void)
+		ChildForm1(void)
 		{
 			InitializeComponent();
 			//
@@ -27,14 +27,14 @@ namespace HelloWorld {
 		/// <summary>
 		/// 使用中のリソースをすべてクリーンアップします。
 		/// </summary>
-		~Form1()
+		~ChildForm1()
 		{
 			if (components)
 			{
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
 	protected: 
 
 	private:
@@ -50,30 +50,30 @@ namespace HelloWorld {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
-			// button1
+			// label1
 			// 
-			this->button1->Location = System::Drawing::Point(94, 162);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"表示";
-			this->button1->UseVisualStyleBackColor = true;
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(76, 121);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(59, 12);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Hello Wold";
 			// 
-			// Form1
+			// ChildForm1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 262);
-			this->Controls->Add(this->button1);
-			this->Name = L"Form1";
-			this->Text = L"Form1";
+			this->Controls->Add(this->label1);
+			this->Name = L"ChildForm1";
+			this->Text = L"ChildForm1";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	};
 }
-
